b567665b9c945bac3629dd49b3d32b791e39dd52
rainniza123 <rainnizaochi123@gmail.com>
2023-09-05 17:16:10 +0700

    update

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnggotaController;
+use App\Http\Controllers\PetugasController;
+use App\Http\Controllers\BukuController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('template.master');
+})->name('blank_page');
+
+// Route::get('/', [AuthorController::class, 'index'])->name('index');
+Route::get('/anggota', [AnggotaController::class, 'create'])->name('anggota');
+Route::get('/buku', [BukuController::class, 'create'])->name('buku');
+Route::get('/petugas', [PetugasController::class, 'create'])->name('petugas');
+
